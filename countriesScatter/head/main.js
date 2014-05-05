9b3a0c245b608ce1faeb84935831075074060495
curran <curran.kelleher@gmail.com>
2014-05-05 18:16:27 -0400

    Started working on countriesScatter

--- countriesScatter/head/main.js
+++ countriesScatter/head/main.js
@@ -1,5 +1,33 @@
 require(['model', 'd3', 'udc'], function (Model, d3, udc) {
-  console.log(Model);
-  console.log(d3);
-  console.log(udc);
+  var udcDataPath = '../../lib/udc-data-v0.0.1/',
+      wppPath = udcDataPath + 'united_nations/world_population_prospects_2012/',
+      wdiPath = udcDataPath + 'world_bank/world_development_indicators/',
+      populationPath = wppPath + 'total_population',
+      concordancePath = wppPath + 'locations',
+      gdpPath = wdiPath + 'GDP_current_USD';
+
+  loadTable(populationPath, function (populationTable) {
+    var populationCube = udc.Cube(populationTable);
+
+    loadTable(gdpPath, function (gdpTable) {
+      var gdpCube = udc.Cube(gdpTable);
+
+      loadTable(concordancePath, function (concordanceTable) {
+        var thesaurus = udc.Thesaurus([concordanceTable]),
+            cube = udc.mergeCubes(populationCube, gdpCube, thesaurus);
+
+        console.log(cube);
+      });
+    });
+  });
+
+  // TODO use promises
+  function loadTable(path, callback){
+    d3.csv(path + '.csv', function (rows) {
+      d3.json(path + '.json', function (table) {
+        table.rows = rows;
+        callback(table);
+      });
+    });
+  }
 });
