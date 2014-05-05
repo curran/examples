4d2f7015900583c17778ea19bfd7df7c3322d38e
curran <curran.kelleher@gmail.com>
2014-05-05 14:52:51 -0400

    Got require config working for countriesScatter example

---
+++ countriesScatter/head/main.js
@@ -0,0 +1,5 @@
+require(['model', 'd3', 'udc'], function (Model, d3, udc) {
+  console.log(Model);
+  console.log(d3);
+  console.log(udc);
+});
---
+++ countriesScatter/head/requireConfig.js
@@ -0,0 +1,8 @@
+require.config({
+  paths: {
+    d3: '../../lib/d3-v3.4.6/d3.min',
+    _: '../../lib/lodash-v2.4.1/dist/lodash.min',
+    model: '../../lib/model-v0.0.4/dist/model.min',
+    udc: '../../lib/udc-v0.0.1/dist/udc',
+  }
+});
