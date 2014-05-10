e7bd810b8d69fef04eaf89dabc0ead8873aa2b1b
curran <curran.kelleher@gmail.com>
2014-05-10 18:48:18 -0400

    Added examples.json listing of examples, used for generating README.md and thumbnails

--- example.js
+++ example.js
@@ -30,9 +30,20 @@ var argv = require('minimist')(process.argv.slice(2)),
     ncp = require('ncp').ncp,
     generateImages = require('./example/generateImages.js'),
     buildREADME = require('./example/buildREADME.js'),
-    methods = {
+    configFile = 'examples.json';
+function run(){
+  fs.readFile(configFile, 'utf8', function (err, data) {
+    var entries = JSON.parse(data);
+    if(err) throw err;
+    var methods = {
       'create': function(exampleName){
         mkdirp(exampleName + '/head');
+        entries.push({
+          name: exampleName,
+          title: exampleName,
+          date: new Date().toString()
+        });
+        write(entries);
       },
       'release': function(exampleName){
         var version = fs.readdirSync(exampleName).filter(function (dir) {
@@ -43,24 +54,24 @@ var argv = require('minimist')(process.argv.slice(2)),
         ncp(src, dest);
       },
       'build': function(){
-        var entries = [
-          {
-            name: 'countriesScatter',
-            latest: 'v1'
-          }
-        ];
-
-        // TODO use listing of examples from examples.json
-        // TODO for each example
-        //entries.forEach(function (entry) {
-        //  var example = entry.name + '/' + entry.latest;
-        //  generateImages(example, function () {
-        //    console.log('Done generating images.');
-        //  });
-        //});
+        entries.forEach(function (entry) {
+          var example = entry.name + '/' + entry.latest;
+          generateImages(example, function () {
+            console.log('Generated images for ' + example);
+          });
+        });
 
         buildREADME(entries);
       },
     };
-
-methods[argv._[0]].apply(null, argv._.slice(1));
+    methods[argv._[0]].apply(null, argv._.slice(1));
+  });
+}
+function write(entries){
+  var json = JSON.stringify(entries, null, 2);
+  fs.writeFile(configFile, json, function(err) {
+    if(err) throw err;
+    console.log("Wrote " + configFile);
+  }); 
+}
+run();
